0f41a78d7ed3eb68e1606628316b5105eb9b02fb
Sarah Poulton <poultonse@gmail.com>
2020-12-28 15:25:20 -0600

    satellite class addition

---
+++ src/app/satellite.ts
@@ -0,0 +1,15 @@
+export class Satellite {
+    name: string; 
+    orbitType: string; 
+    type: string; 
+    operational: boolean; 
+    launchDate: string; 
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
+    }
+}
+// this.name= string;
+//         this.type= string; 
+//         this.launchDate= string;
+//         this.orbitType= string; 
+//         this.operational= boolean
